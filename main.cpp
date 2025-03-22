7c3c9345b730955aaf56fa9e31823e72f7edf8f2
ModernityRejecter <andreidanielciocoiu@gmail.com>
2025-03-22 17:15:07 +0200

    Added wip animations for the player using the angle between the player and cursor

--- main.cpp
+++ main.cpp
@@ -3,16 +3,22 @@
 #include <complex>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <unordered_map>
+#include <cmath>
 
 class Player {
 private:
     sf::Texture texture;
     sf::Sprite sprite;
     float speed;
+    sf::Clock interval;
+    std::unordered_map<int, std::unordered_map<int, sf::Texture>> playerTextures;
+    int textureIndex;
+    int directionIndex;
 
 public:
     Player(const std::string& texturePath, float x, float y, float speed)
-        : texture(texturePath), sprite(texture), speed(speed) {
+        : texture(texturePath), sprite(texture), speed(speed), textureIndex(1), directionIndex(1) {
         if (!texture.loadFromFile(texturePath)) {
             std::cerr << "Eroare la incarcarea imaginii jucatorului!\n";
             exit(EXIT_FAILURE);
@@ -20,11 +26,37 @@ public:
         sprite.setTexture(texture);
         sprite.setPosition(sf::Vector2f(x, y));
     }
+    void loadPlayerTextures() {
+        sf::Texture playerTexture("../assets/plr_sprite_s1.png");
+        playerTextures[1][1] = playerTexture;
+        playerTexture = sf::Texture("../assets/plr_sprite_s2.png");
+        playerTextures[1][2] = playerTexture;
+        playerTexture = sf::Texture("../assets/plr_sprite_w1.png");
+        playerTextures[3][1] = playerTexture;
+        playerTexture = sf::Texture("../assets/plr_sprite_w2.png");
+        playerTextures[3][2] = playerTexture;
+    }
 
     void move(float offsetX, float offsetY) {
         sprite.move({offsetX, offsetY});
     }
+    void idleAnimation() {
+        if (interval.getElapsedTime().asMilliseconds() >= 300) {
+            if (textureIndex == 1) {
+                sprite.setTexture(playerTextures[directionIndex][2]);
+                textureIndex = 2;
+                std::cout <<"am actualizat ";
+            }
+            else {
+                sprite.setTexture(playerTextures[directionIndex][1]);
+                textureIndex = 1;
+                std::cout <<"am actualizat ";
+            }
+            std::cout << textureIndex << std::endl;
+            interval.restart();
+        }
 
+    }
     void update(float deltaTime) {
         sf::Vector2f movement(0.f, 0.f);
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
@@ -36,7 +68,29 @@ public:
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
             movement.x += speed * deltaTime;
 
+        sf::Vector2f position = getPosition();
+        sf::Vector2i mousePosition = sf::Mouse::getPosition();
+
+        float angle = std::atan2(static_cast<float>(mousePosition.y) - position.y, static_cast<float>(mousePosition.x) - position.x) * 180.f / 3.14f;
+        if (angle >= -22.5 && angle < 22.5) {
+            directionIndex = 0; // Dreapta
+        } else if (angle >= 22.5 && angle < 67.5) {
+            directionIndex = 1; // Sus-dreapta
+        } else if (angle >= 67.5 && angle < 112.5) {
+            directionIndex = 2; // Sus
+        } else if (angle >= 112.5 && angle < 157.5) {
+            directionIndex = 3; // Sus-stânga
+        } else if (angle >= 157.5 || angle < -157.5) {
+            directionIndex = 4; // Stânga
+        } else if (angle >= -157.5 && angle < -112.5) {
+            directionIndex = 5; // Jos-stânga
+        } else if (angle >= -112.5 && angle < -67.5) {
+            directionIndex = 6; // Jos
+        } else if (angle >= -67.5 && angle < -22.5) {
+            directionIndex = 7; // Jos-dreapta
+        }
         move(movement.x, movement.y);
+        idleAnimation();
     }
 
     void draw(sf::RenderWindow& window) const {
@@ -109,7 +163,7 @@ private:
 public:
     Game()
         : window(sf::VideoMode({1920, 1111}), "ETERNAL DOOM", sf::Style::Default),
-          player("../assets/plr_sprite1.png", 400.f, 300.f, 200.f) {
+          player("../assets/plr_sprite_s1.png", 400.f, 300.f, 200.f) {
         window.setVerticalSyncEnabled(true);
     }
 
@@ -155,6 +209,7 @@ private:
 
     void update(float deltaTime) {
         player.update(deltaTime);
+        player.loadPlayerTextures();
         for (auto& projectile : projectiles) projectile.update(deltaTime);
         std::erase_if(projectiles, [](const Projectile& b) { return !b.isAlive(); });
     }
