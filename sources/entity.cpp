d9836ace32c14290bdfb68595b7587e0ab4220e1
ModernityRejecter <andreidanielciocoiu@gmail.com>
2025-05-11 01:37:18 +0300

    the definition of insanity is repeating the same action over and over and expecting a different result

--- sources/entity.cpp
+++ sources/entity.cpp
@@ -28,7 +28,7 @@ void Entity::move(float /*deltaTime*/, unsigned int width, unsigned int height) 
         sprite.move(movement);
 }
 Entity::~Entity() = default;
-
+// cppcheck-suppress unusedFunction
 void Entity::shooting(const sf::Vector2f& targetPosition) {
     [[maybe_unused]] float angle = std::atan2((targetPosition.y) - position.y, (targetPosition.x) - position.x) * 180.f / 3.14f;
 
