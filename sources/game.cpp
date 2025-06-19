e28503a9018afbd27cba0a9d2e796427372d7d70
ModernityRejecter <andreidanielciocoiu@gmail.com>
2025-06-20 01:29:57 +0300

    SEND HELP PLEASE

--- sources/game.cpp
+++ sources/game.cpp
@@ -323,3 +323,4 @@ int Game::getScore() const {
 //am nevoie de ceva fisier editat ca sa pot sa dau commit dupa ce am sters fisierul de sfml-subbuild
 //de data asta am sters si cache-ul la cmake
 //daca nici acum nu merge imi pierd mintile
+//am nevoie de ajutor o iau razna
